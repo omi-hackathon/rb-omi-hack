99068c343c7d4e685fd283dbf20bcc17fd89dd9c
Lukas-O <lukas@tribe.wtf>
2018-04-26 15:18:31 +0200

    Interface 2

--- eth/Contracts/ILicensor.sol
+++ eth/Contracts/ILicensor.sol
@@ -10,5 +10,5 @@ contract ILicensor {
     function GetRecording(uint _recordingID) constant public returns (uint, string);
     function GetRecordingByISRC(string _isrc) constant public returns (uint, string);
     function GetLicenseByVideoID(string _videoID) constant public returns (uint, string, uint, uint8, uint8, string);
-    function GetLicenseByUserID(string _userID) constant public returns (uint, string, uint, uint8);
+    function GetLicenseByUserID(string _userID) constant public returns (uint, string, uint, uint8, uint8, string);
 }
