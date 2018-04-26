e90f79766de3e5b18a5585b49fc6edb6b3c10929
Daniel Balcells <dbalcells@gmail.com>
2018-04-26 11:06:31 +0100

    Add underscore to function arguments

--- eth/Contracts/ILicensor.sol
+++ eth/Contracts/ILicensor.sol
@@ -10,14 +10,14 @@ contract ILicensor {
     // RegisterRecordings Array
 
     // public writes
-    function IssueLicense(string userId, uint recordingID) public returns (uint);
-    function LinkToLicense(string videoID, uint licenseID) public returns (bool);
-    function RevokeLicense(uint licenseID) public returns (bool);
+    function IssueLicense(string _userId, uint _recordingID) public returns (uint);
+    function LinkToLicense(string _videoID, uint _licenseID) public returns (bool);
+    function RevokeLicense(uint _licenseID) public returns (bool);
 
     // public reads
-    function GetLicense(uint licenseID) constant public returns (uint, uint, uint, uint8);
-    function GetRecording(uint recordingID) constant public returns (uint, string, uint, uint8);
-    function GetRecordingByISRC(string isrc) constant public returns (uint, string, uint, uint8);
-    function GetRecordingByVideoId(string videoID) constant public returns (uint, string, uint, uint8);
-    function GetRecordingByUserId(string userId) constant public returns (uint, string, uint, uint8);
+    function GetLicense(uint _licenseID) constant public returns (uint, uint, uint, uint8);
+    function GetRecording(uint _recordingID) constant public returns (uint, string, uint, uint8);
+    function GetRecordingByISRC(string _isrc) constant public returns (uint, string, uint, uint8);
+    function GetRecordingByVideoId(string _videoID) constant public returns (uint, string, uint, uint8);
+    function GetRecordingByUserId(string _userId) constant public returns (uint, string, uint, uint8);
 }
