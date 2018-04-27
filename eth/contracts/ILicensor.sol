289744ab6b7b6c352a9c18b92e36c31c98e618ba
Ubuntu <ubuntu@ip-10-0-0-30.eu-west-1.compute.internal>
2018-04-27 10:25:45 +0000

    fix paths

---
+++ eth/contracts/ILicensor.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.19;
+pragma experimental ABIEncoderV2;
+
+contract ILicensor {
+    function RegisterRecording(string _isrc) public returns (uint);
+    function IssueLicense(string _userID, uint _recordingID, uint8 _licenseType) public returns (uint);
+    function LinkToLicense(string _videoID, uint _licenseID) public;
+    function RevokeLicense(uint _licenseID) public;
+    function GetISRCs() view public returns (string);
+    function GetLicense(uint _licenseID) view public returns (uint, string, uint, uint8, uint8, string);
+    function GetRecording(uint _recordingID) view public returns (uint, string);
+    function GetRecordingByISRC(string _isrc) view public returns (uint, string);
+    function GetLicensesByVideoID(string _videoID) view public returns (string);
+    function GetLicensesByUserID(string _userID) view public returns (string);
+}
