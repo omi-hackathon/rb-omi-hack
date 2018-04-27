f9588e70a6498b0abe0524f1e2c7497d45ed6bbc
Mike Christensen <mike_christensen@btinternet.com>
2018-04-27 11:46:14 +0100

    log

--- eth/migrations/1_initial_migration.js
+++ eth/migrations/1_initial_migration.js
@@ -2,5 +2,6 @@ const Migrations = artifacts.require('Migrations');
 
 module.exports = function(deployer) {
     // Deploy the Migrations contract as our only task
+    console.log('OWNER_ADDRESS=', process.env.OWNER_ADDRESS);
     deployer.deploy(Migrations, { gas: 6000000, from: process.env.OWNER_ADDRESS });
 };
--- eth/migrations/2_Licensor_migration.js
+++ eth/migrations/2_Licensor_migration.js
@@ -2,6 +2,7 @@ const Licensor = artifacts.require('Licensor');
 const Utils = artifacts.require('Utils');
 
 module.exports = function(deployer) {
+    console.log('OWNER_ADDRESS=', process.env.OWNER_ADDRESS);
     deployer.deploy(Utils, { gas: 6000000, from: process.env.OWNER_ADDRESS });
     deployer.link(Utils, Licensor);
     deployer.deploy(Licensor, 'https://omi01.docs.apiary.io/', 'RedBull', {
