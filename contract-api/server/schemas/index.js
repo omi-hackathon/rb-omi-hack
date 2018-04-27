4c07899027d5e54d999a2ce3ac2678df76fc28c9
Mike Christensen <mike_christensen@btinternet.com>
2018-04-27 12:14:49 +0100

    dirname

--- contract-api/server/schemas/index.js
+++ contract-api/server/schemas/index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
+const path = require('path');
 
 const schemas = {};
-const filenames = fs.readdirSync('server/schemas'); // eslint-disable-line no-sync
+const filenames = fs.readdirSync(path.resolve(__dirname) + '/server/schemas'); // eslint-disable-line no-sync
 for (const filename of filenames) {
     if (filename === 'index.js') {
         continue;
