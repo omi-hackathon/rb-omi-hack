d8636fa3f26d22d436a1426e564d6e590a9000ce
Mike Christensen <mike_christensen@btinternet.com>
2018-04-27 11:16:16 +0100

    enable parity to start with account unlocked

--- eth/migrations/1_initial_migration.js
+++ eth/migrations/1_initial_migration.js
@@ -1,4 +1,4 @@
-var Migrations = artifacts.require('Migrations');
+const Migrations = artifacts.require('Migrations');
 
 module.exports = function(deployer) {
     // Deploy the Migrations contract as our only task
