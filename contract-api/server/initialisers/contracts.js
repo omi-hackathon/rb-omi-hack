06d7aab0a07318b560739617c56c28bf0d7a75c5
Mike Christensen <mike_christensen@btinternet.com>
2018-04-27 12:19:15 +0100

    update path

--- contract-api/server/initialisers/contracts.js
+++ contract-api/server/initialisers/contracts.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
+const path = require('path');
 const config = require('../config');
 const Contract = require('../utils/contract');
 
 module.exports = async function() {
     const contracts = {};
-    const filenames = fs.readdirSync('../eth/build/contracts'); // eslint-disable-line no-sync
+    const filenames = fs.readdirSync(path.resolve(__dirname) + '../../../eth/build/contracts'); // eslint-disable-line no-sync
     for (const filename of filenames) {
         if (!config.contracts[filename.replace('.json', '')]) {
             console.log('No contract configuration specified for: ', filename.replace('.json', ''));
@@ -13,7 +14,9 @@ module.exports = async function() {
         const contract = config.contracts[filename.replace('.json', '')];
         if (!contract.owner || !contract.address) {
             throw new Error(
-                `Missing owner/address for contract '${filename.replace('.json', '')}' on network ${process.env.ETHEREUM_NETWORK}`,
+                `Missing owner/address for contract '${filename.replace('.json', '')}' on network ${
+                    process.env.ETHEREUM_NETWORK
+                }`,
             );
         }
         contracts[filename.replace('.json', '')] = new Contract(filename, contract.address, contract.owner);
